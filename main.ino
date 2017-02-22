f481dbd66dd5efb8bbbaa6bb60ff52b7a2630094
fu0x11 <francescourbanidue@gmail.com>
2017-02-22 11:15:43 +0100

    added css-like style

--- main.ino
+++ main.ino
@@ -5,8 +5,9 @@
  */
 
 #include <ESP8266WiFi.h>
-// #define LED_PIN 2       	/* LED hooked up on GPIO 2 */
-const int LED_PIN = 2;
+const int LED_PIN = 2;    /* LED hooked up on GPIO 2 */
+const int SHOOTING_TIME = 200;  // ms. the time that the shutter button is being pressed.
+                                // in that time the camera needs to focus and shoot. 
 
 WiFiServer server(80);
 void blinkLED();
@@ -20,7 +21,7 @@ void setup() {
 void loop() {
 
   WiFiClient client = server.available();   /* Check if a client has connected */
-  if (!client) {
+  if (!client){
     return;
   }
 
@@ -32,82 +33,112 @@ void loop() {
   // Match the request
   int val = -1;   /* We'll use 'val' to keep track of both the request type (read/set) and value if set. */
 
-  if (req.indexOf("/shoot") != -1){
-    val = 0;    /* Will write LED low */
-  }
-  else if (req.indexOf("/off") != -1){
-    val = 1;    /* Will write LED high */
-  }
-  else if (req.indexOf("/intervallometer") != -1){
+  if (req.indexOf("/shoot") != -1)
+    val = 0;
+  else if (req.indexOf("/off") != -1)
+    val = 1;
+  else if (req.indexOf("/intervallometer") != -1)
     val = 2;
-  }
-  else if (req.indexOf("/delayed_shoot") != -1){
+  else if (req.indexOf("/delayed_shoot") != -1)
     val = 3;
-  }
-  else if (req.indexOf("/burst") != -1){
+  else if (req.indexOf("/burst") != -1)
     val = 4;
-  }
   /* Otherwise the request is invalid. */
 
-  // if (val >= 0){  /* i.e. val == 0 or val == 1 */
-  //   digitalWrite(LED_PIN, val);   /* Set GPIO 2 according to the request */
-  // }
   switch (val) {
     case 0: // shoot
       digitalWrite(LED_PIN, 0);
-      delay(60);
+      delay(SHOOTING_TIME);
       break;
-    // case 1: // off
-    //   digitalWrite(LED_PIN, 1);
-    //   break;
+    case 1:	// off
+    	digitalWrite(LED_PIN, 1);
+    	break;
     case 2: // intervallometer
       digitalWrite(LED_PIN, 1);
       break;
+
     case 3: // delayed_shoot (2 seconds)
       delay(2000);
       digitalWrite(LED_PIN, 0);
-      delay(60); // ? is it enough?
+      delay(SHOOTING_TIME); // ? is it enough?
       // digitalWrite(LED_PIN, 1);
       break;
+
     case 4: // burst
       digitalWrite(LED_PIN, 0);
       delay(3000);
       // digitalWrite(LED_PIN, 1);
       break;
+
     default:
       digitalWrite(LED_PIN, 1);
   }
 
   client.flush();
 
-  // Prepare the response. Start with the common header:
+  // Prepare the response. Start with the common header and change the rest according to the request:
   String s = "HTTP/1.1 200 OK\r\n";
   s += "Content-Type: text/html\r\n\r\n";
   s += "<!DOCTYPE HTML>\r\n<html>\r\n";
 
+  /* adds some css-styling */
+  s += "<head>" ;
+	s += "<style>" ;
+	// s += "body {background-color: powderblue;}" ;
+	s += "h1 { color: blue; }";
+	s += "p { color: red; }";
+	s += ".button { background-color: #4CAF50; /*green*/ border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; }";
+	s += "</style>";
+	s += "</head>";
+	/* end styling */
+
   s += "<h1>Camera remote</h1>";
 
-  if(val) {
+  if(val == 0){ /* single shoot */
     s += "<p><a href=\"/shoot\"><button>shoot</button></a>&nbsp</p>";
+    s += "<p><a href=\"/intervallometer\"><button>intervallometer</button></a>&nbsp</p>";
+    s += "<p><a href=\"/d-shoot\"><button>delayed shoot</button></a>&nbsp</p>";
+    s += "<p><a href=\"/burst\"><button>burst</button></a>&nbsp</p>";
     val = 1;
   }
-  else if(!val) {
-    s += "<p>LED <a href=\"/off\"><button>ON</button></a>&nbsp;</p>";
+  else if(val == 1){  /* off */ 
+    s += "<p><a href=\"/shoot\"><button>shoot</button></a>&nbsp</p>";
+    s += "<p><a href=\"/intervallometer\"><button>intervallometer</button></a>&nbsp</p>";
+    s += "<p><a href=\"/d-shoot\"><button>delayed shoot</button></a>&nbsp</p>";
+    s += "<p><a href=\"/burst\"><button>burst</button></a>&nbsp</p>";
+    val = 1;
+  }
+  else if(val == 2){  /* intervallometer */
+    s += "<p><a href=\"/shoot\"><button>shoot</button></a>&nbsp</p>";
+    s += "<p><a href=\"/intervallometer\"><button>intervallometer</button></a>&nbsp</p>";
+    s += "<p><a href=\"/d-shoot\"><button>delayed shoot</button></a>&nbsp</p>";
+    s += "<p><a href=\"/burst\"><button>burst</button></a>&nbsp</p>";
+    val = 1;
+  }
+  else if(val == 3){  /* delayed shoot */
+    s += "<p><a href=\"/shoot\"><button>shoot</button></a>&nbsp</p>";
+    s += "<p><a href=\"/intervallometer\"><button>intervallometer</button></a>&nbsp</p>";
+    s += "<p><a href=\"/d-shoot\"><button>delayed shoot</button></a>&nbsp</p>";
+    s += "<p><a href=\"/burst\"><button>burst</button></a>&nbsp</p>";
+    val = 1;
+  }
+  else if(val == 4){  /* burst */
+    s += "<p><a href=\"/shoot\"><button>shoot</button></a>&nbsp</p>";
+    s += "<p><a href=\"/intervallometer\"><button>intervallometer</button></a>&nbsp</p>";
+    s += "<p><a href=\"/d-shoot\"><button>delayed shoot</button></a>&nbsp</p>";
+    s += "<p><a href=\"/burst\"><button>burst</button></a>&nbsp</p>";
+    val = 1;
+  }
+  else {  /* off */
+    // s += "<p>LED <a href=\"/off\"><button>ON</button></a>&nbsp;</p>";
+    s += "<p><a href=\"/shoot\"><button>shoot</button></a>&nbsp</p>";
+    s += "<p><a href=\"/intervallometer\"><button>intervallometer</button></a>&nbsp</p>";
+    s += "<p><a href=\"/d-shoot\"><button>delayed shoot</button></a>&nbsp</p>";
+    s += "<p><a href=\"/burst\"><button>burst</button></a>&nbsp</p>";
     val = 0;
   }
+  s += "</html>\n";		/* close HTML page */
 
-  /* If we're setting the LED, print out a message saying that we made it. */
-  // if (val >= 0) {
-  //   // s += (val) ? "<font size = '330'>LED is now on</font>" : "<font size = '330'>LED is now off</font>";    // FONT DOES NOT WORK
-  //   s += "";
-  // }
-  // else {
-  //   s += "Invalid Request.<br> Try /off or /shoot.";
-  // }
-
-  s += "</html>\n";
-
-  
   client.print(s);  /* Send the response to the client */
                     /* displays the actual webpage */
   
@@ -116,14 +147,14 @@ void loop() {
 }
 
 void setupWiFi() {
-  WiFi.mode(WIFI_AP);
-  const char AP_NameChar[] = "fuckingwificameraconnection";    /* AP id */
-  const char WiFiAPPSK[] = "sparkfun.@2+1";				             /* AP password */
+  WiFi.mode(WIFI_AP);		/* configure esp8266 as access point */
+  const char AP_NameChar[]	= "fuckingwificameraconnection";    /* AP id */
+  const char WiFiAPPSK[]		= "sparkfun.@2+1";				          /* AP password */
   WiFi.softAP(AP_NameChar, WiFiAPPSK);
 }
 
 void initHardware() {
-  Serial.begin(115200);
+  // Serial.begin(115200);
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, HIGH);  // default value high, does not start shooting on startup.
 }
