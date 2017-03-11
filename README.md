249b8ecec2a6643515f78bab14135ec7973fe023
fu0x11 <francescourbanidue@gmail.com>
2017-03-11 15:36:13 +0100

    removed ribbon and minor fix

--- README.md
+++ README.md
@@ -1,19 +1,22 @@
 # Tiny shutter
 
 An hardware device based on the ubiquitous ESP8266 Wi-Fi module intended to give a 
-non-wifi-connected DSLR the ability to shoot remotely, via Wi-Fi connection (access point mode).
+non-wifi-connectable DSLR the ability to shoot remotely, via Wi-Fi connection in access point mode.
 
 [This](https://fu0x11.github.io/tiny-shutter/) is how the UI looks like on a whatsoever browser.
 
 ##### To see it in action:
-* set an [AP_name] and [AP_pass] of your choice [here](https://github.com/fu0x11/tiny-shutter/blob/master/main.ino#L141) and [here](https://github.com/fu0x11/tiny-shutter/blob/master/main.ino#L142) respectively.
+* set an *AP_name* and *AP_pass* of your choice [here](https://github.com/fu0x11/tiny-shutter/blob/master/main.ino#L141) and [here](https://github.com/fu0x11/tiny-shutter/blob/master/main.ino#L142) respectively.
 * upload `main.ino` onto the ESP8266. <sup>1</sup>
-* connect the device you intend to use as remote shutter (e.g. your smartphone) to the Wi-Fi network [AP_name].
+* connect the device you intend to use as remote shutter (e.g. your smartphone) to the Wi-Fi network *AP_name*.
 * open the browser and navigate to: `192.168.4.1`.
 * if no errors occur, you should be able to see [this](https://fu0x11.github.io/tiny-shutter/) interface. The connection is established and you are ready to shoot. :sunglasses:
 
+<!-- <br> -->
+<!-- [This]() is the schematic of the device. -->
 
-<br/><br/>
+
+<br/><br>
 <sup>1</sup> I used an [Arduino Uno](https://www.arduino.cc/en/main/ArduinoBoardUno) to accomplish that. [Here](https://www.youtube.com/watch?v=P_ecAFpUADU) is a step-by-step guide on how to do that.
 
 <br/>
--- main.ino
+++ main.ino
@@ -1,23 +1,23 @@
 /* main.ino
  *
- * Francesco Urbani
+ * francesco urbani
  *
  */
 
 #include <ESP8266WiFi.h>
 
 /* define of commands used to trigger the shutter. 
- * depending on whether I use the optocoupler or I connect the camera directly to GPIO2, these two needs to be swapped.
+ * depending on whether I use the optocoupler or I connect the camera directly to GPIO0, these two needs to be swapped.
  */
-#define TRIGGER 1   /* TRIGGER = 0, shoot occurs if GPIO2 is attached to the camera. */
+#define TRIGGER 1   /* TRIGGER = 0, shoot occurs if GPIO0 is attached to the camera. */
                     /* TRIGGER = 1, shoot occurs if the optocoupler is involved. */
 #define HALT    0
 
-#define INDEX  "<!DOCTYPE html> <head> <html lang='en'> <meta charset='UTF-8'> <meta name='viewport' content='width=device-width, initial-scale=1.0'/> <link rel='icon' type='image/png' href='img/dslr_favicon.png'/> <title>Tiny shutter</title> <style> h1 { font-family: arial; color: blue; text-align: center; font-size: 20px; } button { font-family: arial; box-shadow: 0 12px 36px 0 rgba(0,0,0,0.24), 0 20px 50px 0 rgba(0,0,0,0.19); width: 100%; height: 80px; /* height of a single button*/ border: none; color: #d8d8d8; text-align: center; font-size: 30px; background-color: #3b02ff; } #shootButton { background-color: #0a0aae; } #intervallometerButton { background-color: #3fc5ff; } #copy { font-size: 10px; margin-top: 1cm; font-family: monospace; text-align: center; } </style> </head> <!-- <h1>Camera remote</h1> --> <!-- fork me on GitHub ribbon --> <a href='https://github.com/fu0x11/camera-shutter-esp8266' target='_blank'><img style='position: absolute; top: 0; right: 0; border: 0; height: 2.6cm; width: 2.6cm' src='img/forkme.png' data-canonical-src=''></a> <body> <p><a href=index.html><button id='shootButton'>SINGLE SHOOT</button></a></p> <!-- /shoot --> <!-- index.html --> <p><a href=index.html><button>DELAYED SHOOT</button></a></p> <!-- /delshoot --> <!-- index.html --> <p><a href=index.html><button>BURST</button></a></p> <!-- /burst --> <!-- index.html --> <p><a href=index.html><button>DELAYED BURST</button></a></p> <!-- /delburst --> <!-- index.html --> <p><a href=interv.html><button id='intervallometerButton'>INTERVALLOMETER</button></a></p> <!-- /interv --> <!-- innterv.html --> </body> <!-- copy --> <div id='copy'> &copy francesco urbani </div> <!-- end copy --> </html> "
-#define INTERV "<!DOCTYPE html> <head> <html lang='en'> <meta charset='UTF-8'> <meta name='viewport' content='width=device-width, initial-scale=1.0'/> <link rel='icon' type='image/png' href='img/dslr_favicon.png'/> <title>Tiny shutter</title> <style> h1 { font-family: arial; color: #ff0000; text-align: center; font-size: 20px; } button { font-family: arial; box-shadow: 0 12px 36px 0 rgba(0,0,0,0.24), 0 20px 50px 0 rgba(0,0,0,0.19); width: 100%; height: 464px; /* result of (height of a button)*(number of buttons)+(default button distance)*(number of distances) */ /* 80 * 5 + 16 * 4 */ border: none; color: #d8d8d8; text-align: center; font-size: 80px; background-color: #00DD00; } #offButton { background-color: #ff0000; } #copy { font-size: 10px; margin-top: 1cm; font-family: monospace; text-align: center; } </style> </head> <!-- fork me on GitHub ribbon --> <a href='https://github.com/fu0x11/camera-shutter-esp8266' target='_blank'><img style='position: absolute; top: 0; right: 0; border: 0; height: 2.6cm; width: 2.6cm' src='img/forkme.png' data-canonical-src=''></a> <!-- <h1>Camera remote</h1> --> <body> <p><a href=index.html><button id='offButton'>OFF</button></a></p> <!-- /off --> <!-- index.html --> <!-- copy --> <div id='copy'> &copy francesco urbani </div> <!-- end copy --> </body> </html> "
+#define INDEX  "<!DOCTYPE html> <head> <html lang='en'> <meta charset='UTF-8'> <meta name='viewport' content='width=device-width, initial-scale=1.0'/> <link rel='icon' type='image/png' href='img/dslr_favicon.png'/> <title>Tiny shutter</title> <style> h1 { font-family: arial; color: blue; text-align: center; font-size: 20px; } button { font-family: arial; box-shadow: 0 12px 36px 0 rgba(0,0,0,0.24), 0 20px 50px 0 rgba(0,0,0,0.19); width: 100%; height: 80px; /* height of a single button*/ border: none; color: #d8d8d8; text-align: center; font-size: 30px; background-color: #3b02ff; } #shootButton { background-color: #0a0aae; } #intervallometerButton { background-color: #3fc5ff; } #copy { font-size: 8px; margin-top: 1cm; font-family: monospace; text-align: center; } </style> </head> <!-- <h1>Camera remote</h1> --> <!-- fork me on GitHub ribbon --> <!-- <a href='https://github.com/fu0x11/camera-shutter-esp8266' target='_blank'><img style='position: absolute; top: 0; right: 0; border: 0; height: 2.6cm; width: 2.6cm' src='img/forkme.png' data-canonical-src=''></a> --> <body> <p><a href=index.html><button id='shootButton'>SINGLE SHOOT</button></a></p> <!-- /shoot --> <!-- index.html --> <p><a href=index.html><button>DELAYED SHOOT</button></a></p> <!-- /delshoot --> <!-- index.html --> <p><a href=index.html><button>BURST</button></a></p> <!-- /burst --> <!-- index.html --> <p><a href=index.html><button>DELAYED BURST</button></a></p> <!-- /delburst --> <!-- index.html --> <p><a href=interv.html><button id='intervallometerButton'>INTERVALLOMETER</button></a></p> <!-- /interv --> <!-- innterv.html --> </body> <!-- copy --> <div id='copy'> &copy francesco urbani </div> <!-- end copy --> </html> "
+#define INTERV "<!DOCTYPE html> <head> <html lang='en'> <meta charset='UTF-8'> <meta name='viewport' content='width=device-width, initial-scale=1.0'/> <link rel='icon' type='image/png' href='img/dslr_favicon.png'/> <title>Tiny shutter</title> <style> h1 { font-family: arial; color: #ff0000; text-align: center; font-size: 20px; } button { font-family: arial; box-shadow: 0 12px 36px 0 rgba(0,0,0,0.24), 0 20px 50px 0 rgba(0,0,0,0.19); width: 100%; height: 464px; /* result of (height of a button)*(number of buttons)+(default button distance)*(number of distances) */ /* 80 * 5 + 16 * 4 */ border: none; color: #d8d8d8; text-align: center; font-size: 80px; background-color: #00DD00; } #offButton { background-color: #ff0000; } #copy { font-size: 8px; margin-top: 1cm; font-family: monospace; text-align: center; } </style> </head> <!-- fork me on GitHub ribbon --> <!-- <a href='https://github.com/fu0x11/camera-shutter-esp8266' target='_blank'><img style='position: absolute; top: 0; right: 0; border: 0; height: 2.6cm; width: 2.6cm' src='img/forkme.png' data-canonical-src=''></a> --> <!-- <h1>Camera remote</h1> --> <body> <p><a href=index.html><button id='offButton'>OFF</button></a></p> <!-- /off --> <!-- index.html --> <!-- copy --> <div id='copy'> &copy francesco urbani </div> <!-- end copy --> </body> </html> "
 
-const int SHOOT_PIN = 0;    /* LED hooked up on GPIO 0 */
-const int SHOOTING_TIME = 600;  // ms. the time that the shutter button is being pressed.
+const int SHOOT_PIN = 2;    /* LED hooked up on GPIO 0 */
+const int SHOOTING_TIME = 500;  // ms. the time that the shutter button is being pressed.
                                 // in that time the camera needs to focus and shoot.
 const int INTERV_TIME = 5000-SHOOTING_TIME;   // intervallometer time = 5 seconds.
 
@@ -87,7 +87,6 @@ void loop() {
       s += INTERV;
       client.flush();
       client.print(s);
-      repeatIntervallometer:
       intervallometer();
       break;
     case 3: // delayed_shoot
@@ -135,6 +134,9 @@ void loop() {
 
   Serial.println("Client disonnected");  /* The client will actually be disconnected when the function returns and 'client' object is detroyed */
 }
+
+
+
 
 void setupWiFi() {
   WiFi.mode(WIFI_AP);   /* configure esp8266 as access point */
